c5580c1c4267c5d1177dfa0640643a443f66aa8f
tian-xiaogu <2257736387@qq.com>
2023-12-05 20:23:03 +0800

    fix:修复跨域问题

---
+++ src/main/java/com/config/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package com.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 解决跨域
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                .allowedHeaders("*")
+                .maxAge(3600)
+                .allowCredentials(true);
+    }
+}
